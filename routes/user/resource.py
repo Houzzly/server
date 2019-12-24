754885d3911d32bf100ae5aff88d48c17f81c005
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-24 12:32:50 +0000

    feat(server): add user register resource class

--- routes/user/resource.py
+++ routes/user/resource.py
@@ -13,3 +13,15 @@ class UserRegister(Resource):
                         help="Email is required")
     parser.add_argument("password", type=str, required=True,
                         help="Password is required")
+
+    def post(self):
+        data = self.parser.parse_args()
+
+        if UserModel.find_by_email(data["email"]):
+            return {"message": "User with email of {} already exists".format(data['email'])}
+
+        user = UserModel(data["firstName"], data['lastName'],
+                         data['email'], data['password'])
+        user.save_to_db()
+
+        return {"message": "User created successfully"}, 201
