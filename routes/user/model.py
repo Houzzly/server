94250ccf1d36a3b688b0ccd9bacf86fe76267de4
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-24 12:21:37 +0000

    feat(server): create user model file

---
+++ routes/user/model.py
@@ -0,0 +1,27 @@
+import sqlite3
+from db import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def find_by_email(self, email):
+        return self.query.filter_by(email=email).first()
+
+    @classmethod
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
