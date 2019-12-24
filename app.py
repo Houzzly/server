e062315d193f012abfd0bd68499d2a3209912c15
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-24 11:45:48 +0000

    feat(server): add environment variables

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import os
 
-from db import db
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
+from keys import SECRET_KEY
+
+# app config and api setup
 app = Flask(__name__)
+@app.route("/api", methods=["GET"])
+def get_api_status():
+    return "Api v1"
+
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", 'sqlite:///data.db')
+app.secret_key = os.environ.get("SECRET_KEY", SECRET_KEY)
+api = Api(app)
 
 if __name__ == "__main__":
     db.init_app(app)
